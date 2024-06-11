da7fe3cf8a324efd0e6c531ff391461456586853
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-11 17:58:05 +0200

    end

--- js/main.js
+++ js/main.js
@@ -2,5 +2,18 @@
 const listaDellaSpesa = ['burro', 'latte', 'uova', 'pomodori', 'zucchine', 'formaggio', 'affettati', 'pane', 'biscotti', 'pasta','acqua'];
 console.log(listaDellaSpesa);
 //creare un ciclo usando while per leggere il contenuto della mia array
+let item = 0;
+while (item < listaDellaSpesa.length){
+    console.log(listaDellaSpesa[item]);
+    item++;
+    let foglio = document.querySelector('#contitore');
+    let itemFoglio = `
+            <div class="lista_spesa" > 
+            ${listaDellaSpesa[item]}
+            </div>`;
+    foglio.innerHTML += itemFoglio;
+}
+
+
 
 //stamparlo in pagina 
