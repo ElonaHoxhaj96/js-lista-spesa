a168414fa5159a29ef59d12ddb400cd5e02245b2
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-11 17:20:06 +0200

    feat: entered the array list element in JS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//creare una array con la lista della spesa
+const listaDellaSpesa = ['burro', 'latte', 'uova', 'pomodori', 'zucchine', 'formaggio', 'affettati', 'pane', 'biscotti', 'pasta','acqua'];
+console.log(listaDellaSpesa);
+//creare un ciclo usando while per leggere il contenuto della mia array
+
+//stamparlo in pagina 
